8c41553c660cfb90259a8f45b667568ea98e3fd6
GurkeeratChhina <gurkeerat.chhina@gmail.com>
2025-12-04 02:30:00 -0800

    attempting to use single pass and queue, small error somewhere

--- AdventOfCode/Day04.cs
+++ AdventOfCode/Day04.cs
@@ -11,7 +11,7 @@ public class Day04 : BaseDay
 
     public override ValueTask<string> Solve_1() => new($"{Part1()}");
 
-    public override ValueTask<string> Solve_2() => new($"{Part2()}");
+    public override ValueTask<string> Solve_2() => new($"{Part2Faster()}");
 
     public int Part1(){
         int height = _input.Length;
@@ -95,4 +95,112 @@ public class Day04 : BaseDay
         
         return total;
     }
+
+    public int Part2Faster(){
+        int height = _input.Length;
+        int width = _input[0].Length;
+        int total = 0;
+        int count = 0;
+        int surrounding;
+        int[,] grid = new int[height, width]; //we will use int grid where 0 = `.`, 1 = `@`
+
+        //populate grid with @'s from _input
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                if (_input[i][j] == '@') {
+                    grid[i,j] = 1;
+                }
+            }
+        }
+        
+        //as long as we get a non-zero answer, repeat part1
+        do {
+            count = 0;
+            for(int i = 0; i < height; i++) {
+                for (int j = 0; j < width; j++) {
+                    if (grid[i,j] > 0) {
+                        surrounding = 0;
+                        for (int dx = -1; dx <= 1; dx++) {
+                            for (int dy = -1; dy <= 1; dy++) {
+                                if (i + dy >= 0 && i + dy < height && j + dx >= 0 && j + dx < width && grid[i+dy,j+dx] > 0) {
+                                    surrounding++;
+                                }
+                            }
+                        }
+                        if (surrounding <5) {
+                            count++;
+                            grid[i,j] = 0;
+                        }
+                    }
+                }
+            }
+            total += count;
+        } while (count > 0);
+        
+        return total;
+    }
+
+    public int Part2Faster2(){
+        int height = _input.Length;
+        int width = _input[0].Length;
+        int count = 0;
+        int surrounding;
+        int[,] grid = new int[height, width]; //
+        (int,int)[] dirs ={(-1,-1), (0,-1), (1,-1), (-1, 0), (1, 0), (-1, 1), (0, 1), (1, 1)};
+        int nx;
+        int ny;
+        Queue<(int, int)> Q = new();
+
+        void addNeighboursToQueue(int y, int x) {
+            foreach (var (dx, dy) in dirs) {
+                nx = x + dx;
+                ny = y + dy;
+                if (ny >= 0 && ny < height && nx >= 0 && nx < width && grid[ny,nx] >0) {
+                    Q.Enqueue((ny,nx));
+                }
+            }
+        }
+
+        //populate grid, replacing @ with the number of neighbouring @'s
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                if (_input[i][j] == '@') {
+                    surrounding = 0;
+                    foreach (var (dx, dy) in dirs) {
+                        nx = j + dx;
+                        ny = i + dy;
+                        if (ny >= 0 && ny < height && nx >= 0 && nx < width && _input[ny][nx] == '@') {
+                            surrounding++;
+                        }
+                    }
+                    grid[i,j] = surrounding;
+                }
+            }
+        }
+
+        for(int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                if (grid[i,j] > 0 && grid[i,j] < 4) {
+                    count++;
+                    grid[i,j] = 0;
+                    addNeighboursToQueue(i,j);
+
+                    while(Q.Count > 0) {
+                        var (y,x) = Q.Dequeue();
+                        if (grid[y,x] >0 & grid[y,x] <= 4) {
+                            count++;
+                            grid[y,x] = 0;
+                            addNeighboursToQueue(y,x);
+                        } else {
+                            grid[y,x]--;
+                        }
+                    }
+                }
+            }
+        }
+        
+        return count;
+    }
+
+    
 }
