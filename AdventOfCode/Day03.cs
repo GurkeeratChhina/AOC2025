78172c0821da9936fb6e197a4ba8e5ba30359515
GurkeeratChhina <gurkeerat.chhina@gmail.com>
2025-12-03 21:49:56 -0800

    both parts done, p2 is slower than desired

--- AdventOfCode/Day03.cs
+++ AdventOfCode/Day03.cs
@@ -11,7 +11,7 @@ public class Day03 : BaseDay
 
     public override ValueTask<string> Solve_1() => new($"{Part1()}");
 
-    public override ValueTask<string> Solve_2() => new($"{Part2WithoutStrings()}");
+    public override ValueTask<string> Solve_2() => new($"{Part2Attempt5()}");
 
     public int Part1(){
         int sum = 0;
@@ -76,6 +76,94 @@ public class Day03 : BaseDay
         int numDigits = 12;
         foreach (var line in _input) {
             char[] bestSoFar = new char[numDigits];
+            for (int i = 0; i < line.Length; i++) {
+                for (int j = 0; j < numDigits; j++) {
+                    if (line.Length-i < numDigits -j) {
+                        continue;
+                    }
+                    if (line[i] > bestSoFar[j]) {
+                        bestSoFar[j] = line[i];
+                        for (int k = j+1; k < numDigits; k++) {
+                            bestSoFar[k] = '0';
+                        }
+                        break;
+                    }
+                }
+            }
+            long number = 0;
+            for (int i = 0; i < numDigits; i++) {
+                number = number * 10 + (bestSoFar[i] - '0');
+            }
+            sum += number;
+        }
+        return sum;
+    }
+
+    public long Part2WithoutChars() {
+        long sum = 0;
+        int numDigits = 12;
+        
+        long[] pow10 = new long[numDigits + 1];
+        pow10[0] = 1;
+        for (int i = 1; i <= numDigits; i++) {
+            pow10[i] = pow10[i-1] * 10;
+        }
+
+        foreach (var line in _input) {
+            long bestSoFar = 0;
+            for (int i = 0; i < line.Length; i++) {
+                int digit = line[i] - '0';
+                for (int j = 0; j < numDigits; j++) {
+                    if (line.Length-i < numDigits -j) {
+                        continue;
+                    }
+                    if (digit > (bestSoFar/ pow10[numDigits-j-1])%10) {
+                        bestSoFar = ((bestSoFar/ pow10[numDigits-j])*10 + digit)*pow10[numDigits-j-1];
+                        break;
+                    }
+                }
+            }
+            sum += bestSoFar;
+        }
+        return sum;
+    }
+
+    public long Part2Attempt4() {
+        long sum = 0;
+        int numDigits = 12;
+        Span<int> bestSoFar = stackalloc int[numDigits];
+        foreach (var line in _input) {
+            bestSoFar[0] = 0;
+            for (int i = 0; i < line.Length; i++) {
+                int digit = line[i] - '0';
+                for (int j = 0; j < numDigits; j++) {
+                    if (line.Length-i < numDigits -j) {
+                        continue;
+                    }
+                    if (digit > bestSoFar[j]) {
+                        bestSoFar[j] = digit;
+                        for (int k = j+1; k < numDigits; k++) {
+                            bestSoFar[k] = 0;
+                        }
+                        break;
+                    }
+                }
+            }
+            long number = 0;
+            for (int i = 0; i < numDigits; i++) {
+                number = number * 10 + bestSoFar[i];
+            }
+            sum += number;
+        }
+        return sum;
+    }
+
+    public long Part2Attempt5() {
+        long sum = 0;
+        int numDigits = 12;
+        Span<char> bestSoFar = stackalloc char[numDigits];
+        foreach (var line in _input) {
+            bestSoFar[0] = '0';
             for (int i = 0; i < line.Length; i++) {
                 for (int j = 0; j < numDigits; j++) {
                     if (line.Length-i < numDigits -j) {
---
+++ AdventOfCode/Day04.cs
@@ -0,0 +1,98 @@
+﻿namespace AdventOfCode;
+
+public class Day04 : BaseDay
+{
+    private readonly string[] _input;
+
+    public Day04()
+    {
+        _input = File.ReadAllLines(InputFilePath);
+    }
+
+    public override ValueTask<string> Solve_1() => new($"{Part1()}");
+
+    public override ValueTask<string> Solve_2() => new($"{Part2()}");
+
+    public int Part1(){
+        int height = _input.Length;
+        int width = _input[0].Length;
+        int count = 0;
+        int surrounding;
+        // Console.WriteLine(height + " " + width);
+        for(int i = 0; i < height; i++) {
+            // Console.WriteLine("row " + i + " is equal to " + _input[i]);
+            for (int j = 0; j < width; j++) {
+                // Console.WriteLine("looking at column " + j);
+                if (_input[i][j] == '@') {
+                    // Console.WriteLine("found an @");
+                    surrounding = 0;
+                    for (int dx = -1; dx <= 1; dx++) {
+                        for (int dy = -1; dy <= 1; dy++) {
+                            if (i + dy >= 0 && i + dy < height && j + dx >= 0 && j + dx < width && _input[i+dy][j+dx] == '@') {
+                                surrounding++;
+                            }
+                        }
+                    }
+                    if (surrounding <5) {
+                        count++;
+                    }
+                    // Console.WriteLine("evaluated column " + j + " new count is " + count);
+                }
+            }
+        }
+        return count;
+    }
+
+    public int Part2(){
+        int height = _input.Length;
+        int width = _input[0].Length;
+        int total = 0;
+        int count = 0;
+        int surrounding;
+        int[,] grid = new int[height, width]; //we will use int grid where 0 = `.`, 1 = `X`, and 2 = `@`.
+
+        //populate grid with @'s from _input
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                if (_input[i][j] == '@') {
+                    grid[i,j] = 2;
+                }
+            }
+        }
+        
+        //as long as we get a non-zero answer, repeat part1, marking 2's that can be removed as 1
+        do {
+            count = 0;
+            for(int i = 0; i < height; i++) {
+                for (int j = 0; j < width; j++) {
+                    if (grid[i,j] == 2) {
+                        surrounding = 0;
+                        for (int dx = -1; dx <= 1; dx++) {
+                            for (int dy = -1; dy <= 1; dy++) {
+                                if (i + dy >= 0 && i + dy < height && j + dx >= 0 && j + dx < width && (grid[i+dy,j+dx] == 2 || grid[i+dy,j+dx] == 1)) {
+                                    surrounding++;
+                                }
+                            }
+                        }
+                        if (surrounding <5) {
+                            count++;
+                            grid[i,j] = 1;
+                        }
+                    }
+                }
+            }
+            total += count;
+            
+            //remove any 1's
+            for (int i = 0; i < height; i++) {
+                for (int j = 0; j < width; j++) {
+                    if (grid[i,j] == 1) {
+                        grid[i,j] = 0;
+                    }
+                }
+            }
+        } while (count > 0);
+        
+        return total;
+    }
+}
