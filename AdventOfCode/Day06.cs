b759c52a2217e5c90be26032854e1584fe2b24a1
GurkeeratChhina <gurkeerat.chhina@gmail.com>
2025-12-06 00:13:53 -0800

    refactored and deleted old method, but slower due to parsing time being counted now

--- AdventOfCode/Day06.cs
+++ AdventOfCode/Day06.cs
@@ -3,144 +3,99 @@
 public class Day06 : BaseDay
 {
     private readonly string[] _input;
-    List<string> nums1;
-    List<string> nums2;
-    List<string> nums3;
-    List<string> nums4;
-    List<string> ops;
 
     public Day06()
     {
         _input = File.ReadAllLines(InputFilePath);
 
-        string line1 = _input[0];
-        string line2 = _input[1];
-        string line3 = _input[2];
-        string line4 = _input[3];
-        string line5 = _input[4];
-
-        nums1 = new List<string>();
-        nums2 = new List<string>();
-        nums3 = new List<string>();
-        nums4 = new List<string>();
-        ops = new List<string>();
-
-        string string1 = "";
-        string string2 = "";
-        string string3 = "";
-        string string4 = "";
-        string string5 = "";
-        for (int i = 0; i<line1.Length; i++) {
-            if (line1[i] == ' ' && line2[i] == ' ' && line3[i] == ' ' && line4[i] == ' ' && line5[i] == ' ') {
-                nums1.Add(string1);
-                nums2.Add(string2);
-                nums3.Add(string3);
-                nums4.Add(string4);
-                ops.Add(string5);
-
-                string1 = "";
-                string2 = "";
-                string3 = "";
-                string4 = "";
-                string5 = "";
-            } else {
-                if (line1[i] == ' ') {
-                    // string1 += '0';
-                } else {
-                    string1 += line1[i];
-                }
-
-                if (line2[i] == ' ') {
-                    // string2 += '0';
-                } else {
-                    string2 += line2[i];
-                }
-
-                if (line3[i] == ' ') {
-                    // string3 += '0';
-                } else {
-                    string3 += line3[i];
-                }
-
-                if (line4[i] == ' ') {
-                    // string4 += '0';
-                } else {
-                    string4 += line4[i];
-                }
-
-                if (line5[i] == ' ') {
-                } else {
-                    string5 += line5[i];
-                }
-            }
-            
-        }
-        nums1.Add(string1);
-        nums2.Add(string2);
-        nums3.Add(string3);
-        nums4.Add(string4);
-        ops.Add(string5);
     }
 
-    public override ValueTask<string> Solve_1() => new($"{Part1()}");
+    public override ValueTask<string> Solve_1() => new($"{Part1Refactored()}");
 
     public override ValueTask<string> Solve_2() => new($"{Part2()}");
 
-    public long Part1(){
+    public long Part1Refactored() {
         long total = 0;
-        long num1;
-        long num2;
-        long num3;
-        long num4;
+        long subtotal;
 
-        for (int i = 0; i < ops.Count; i++) {
-            num1 = long.Parse(nums1[i].Trim('0'));
-            num2 = long.Parse(nums2[i].Trim('0'));
-            num3 = long.Parse(nums3[i].Trim('0'));
-            num4 = long.Parse(nums4[i].Trim('0'));
-            
-            // Console.WriteLine(nums1[i] + " turns into " + num1);
-            // Console.WriteLine(ops[i]);
+        int numberRows = _input.Length -1;
 
-            if (ops[i] == "+") {
-                // Console.WriteLine("adding");
-                total += num1 + num2 + num3 + num4;
+        string operations = _input[^1].Replace(" ", "");
+        List<string>[] nums = new List<string>[numberRows]; 
 
+        for (int i = 0; i < numberRows; i++) {
+            nums[i] = _input[i].Split(" ", StringSplitOptions.RemoveEmptyEntries).ToList();
+            // Console.WriteLine("{0}", nums[i]);
+        }
+
+        for (int i = 0; i < operations.Length; i++) {         
+            if (operations[i] == '+') {
+                subtotal = 0;
+                for (int j = 0; j < numberRows; j++) {
+                    subtotal += long.Parse(nums[j][i]);
+                }
             } else {
-                total += num1 * num2 * num3 * num4;
+                subtotal = 1;
+                for (int j = 0; j < numberRows; j++) {
+                    subtotal *= long.Parse(nums[j][i]);
+                }
             }
-            // Console.WriteLine(total);
+            total += subtotal;
         }
 
         return total;
     }
 
-    public long Part1Refactored() {
+    public long Part1RefactoredSlower() {
         long total = 0;
         long subtotal;
-        List<long> nums = new List<long>();
-
-        string operations = _input[^1].Replace(" ", ""); 
+        char prevOp = '+';
+        int numberRows = _input.Length -1;
+        string operations = _input[^1];
+        string[] nums = new string[numberRows];
+        for (int j = 0; j < numberRows; j++) {
+            nums[j] = "0";
+        }
 
         for (int i = 0; i < operations.Length; i++) {
-            nums.Add(long.Parse(nums1[i]));
-            nums.Add(long.Parse(nums2[i]));
-            nums.Add(long.Parse(nums3[i]));
-            nums.Add(long.Parse(nums4[i]));
-            
-            if (operations[i] == '+') {
-                subtotal = 0;
-                foreach (var num in nums) {
-                    subtotal += num;
+            if (operations[i] == '+' || operations[i] == '*') {
+                // Console.WriteLine(prevOp);
+                if (prevOp == '+') {
+                    subtotal = 0;
+                    for (int j = 0; j < numberRows; j++) {
+                        subtotal += long.Parse(nums[j]);
+                        nums[j] = "";
+                    }
+                } else {
+                    subtotal = 1;
+                    for (int j = 0; j < numberRows; j++) {
+                        subtotal *= long.Parse(nums[j]);
+                        nums[j] = "";
+                    }
                 }
-            } else {
-                subtotal = 1;
-                foreach (var num in nums) {
-                    subtotal *= num;
-                }
+                total += subtotal;
+                prevOp = operations[i];
             }
-            total += subtotal;
+
+            for (int j = 0; j < numberRows; j++) {
+                nums[j] += _input[j][i];
+            }
         }
+
+        if (prevOp == '+') {
+            subtotal = 0;
+            for (int j = 0; j < numberRows; j++) {
+                subtotal += long.Parse(nums[j]);
+                nums[j] = "";
+            }
+        } else {
+            subtotal = 1;
+            for (int j = 0; j < numberRows; j++) {
+                subtotal *= long.Parse(nums[j]);
+                nums[j] = "";
+            }
+        }
+        total += subtotal;
 
         return total;
     }
