b98d644c5e7648bb2dfa6732bbfc1c179d7199e7
GurkeeratChhina <gurkeerat.chhina@gmail.com>
2025-12-07 21:18:30 -0800

    d5 optimized via sorting array of indices rather than original array

--- AdventOfCode/Day05.cs
+++ AdventOfCode/Day05.cs
@@ -3,14 +3,24 @@
 public class Day05 : BaseDay
 {
     private readonly string[] _input;
-    List<(long low, long high)> ranges;
-    List<long> food;
+    long[] lows;
+    long[] highs;
+    byte[] indices;
+    long[] food;
+    short[] foodIndices;
 
     public Day05()
     {
         _input = File.ReadAllLines(InputFilePath);
-        ranges = new List<(long,long)>();
-        food = new List<long>();
+    }
+
+    public override ValueTask<string> Solve_1() => new($"{Part1()}");
+
+    public override ValueTask<string> Solve_2() => new($"{Part2()}");
+
+    public void parseCustom() {
+        List<long> tempLows = new List<long>();
+        List<long> tempHighs = new List<long>();
 
         int split = 0;
         for (; split < _input.Length; split++) {
@@ -19,29 +29,40 @@ public class Day05 : BaseDay
                 break;
             }
             string[] halves = line.Split("-");
-            ranges.Add((long.Parse(halves[0]), long.Parse(halves[1])));
+            tempLows.Add(long.Parse(halves[0]));
+            tempHighs.Add(long.Parse(halves[1]));
+        }
+        lows = tempLows.ToArray();
+        highs = tempHighs.ToArray();
+
+        indices = new byte[split];
+        food = new long[_input.Length - split - 1];
+        for (byte i = 0; i < split; i++) {
+            indices[i] = i;
         }
         for (int i = split+1; i < _input.Length; i++) {
-            food.Add(long.Parse(_input[i]));
+            food[i-split-1] = long.Parse(_input[i]);
+        }
+        foodIndices = new short[food.Length];
+        for (short i = 0; i < foodIndices.Length; i++) {
+            foodIndices[i] = i;
         }
     }
 
-    public override ValueTask<string> Solve_1() => new($"{Part1()}");
-
-    public override ValueTask<string> Solve_2() => new($"{Part2()}");
-
     public int Part1(){
+        parseCustom();
+
         int count = 0;
         
-        ranges.Sort();
-        food.Sort();
+        foodIndices.Sort(customComparison2);
+        indices.Sort(customTupleComparison);
 
-        int current_index = 0;
-        foreach (var item in food) {
-            while( current_index < ranges.Count && ranges[current_index].high < item) {
+        short current_index = 0;
+        foreach (var i in foodIndices) {
+            while(current_index < indices.Length && highs[indices[current_index]] < food[i]) {
                 current_index++;
             }
-            if (current_index < ranges.Count && item >= ranges[current_index].low) {
+            if (current_index < indices.Length && food[i] >= lows[indices[current_index]]) {
                 count++;
             }
         }
@@ -54,18 +75,36 @@ public class Day05 : BaseDay
         long prev_low = -1;
         long prev_high = -1;
 
-        ranges.Sort();
-
-        foreach (var range in ranges) {
-            if (range.low <= prev_high) {
-                prev_high = Math.Max(prev_high, range.high);
+        foreach (var index in indices) {
+            if (lows[index] <= prev_high) {
+                prev_high = Math.Max(prev_high, highs[index]);
             } else {
                 count += prev_high - prev_low + 1;
-                prev_low = range.low;
-                prev_high = range.high;
+                prev_low = lows[index];
+                prev_high = highs[index];
             }
         }
         count += prev_high - prev_low + 1;
         return count-1;
+    }
+
+    public int customTupleComparison( byte a, byte b) {
+        if (lows[a] == lows[b]) {
+            return 0;
+        }
+        if (lows[a] < lows[b]) {
+            return -1;
+        } 
+        return 1;
+    }
+
+    public int customComparison2(short a, short b) {
+        if (food[a] == food[b]) {
+            return 0;
+        }
+        if (food[a] < food[b]) {
+            return -1;
+        } 
+        return 1;
     }
 }
---
+++ AdventOfCode/Day08.cs
@@ -0,0 +1,23 @@
+﻿namespace AdventOfCode;
+
+public class Day08 : BaseDay
+{
+    private readonly string[] _input;
+
+    public Day08()
+    {
+        _input = File.ReadAllLines(InputFilePath);
+    }
+
+    public override ValueTask<string> Solve_1() => new($"{Part1()}");
+
+    public override ValueTask<string> Solve_2() => new($"{Part2()}");
+
+    public int Part1(){
+        return 0;
+    }
+
+    public int Part2(){
+        return 0;
+    }
+}
