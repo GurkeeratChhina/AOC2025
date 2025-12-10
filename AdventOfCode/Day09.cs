9cbd9859a1607479044c47655309587eb34068d9
GurkeeratChhina <gurkeerat.chhina@gmail.com>
2025-12-10 15:22:29 -0800

    d10 p1 slow brute force with some memoization

--- AdventOfCode/Day09.cs
+++ AdventOfCode/Day09.cs
@@ -3,12 +3,14 @@
 public class Day09 : BaseDay
 {
     private readonly string[] _input;
+    private int n;
     private int[] xs;
     private int[] ys;
 
     public Day09()
     {
         _input = File.ReadAllLines(InputFilePath);
+        n = _input.Length;
         xs = new int[_input.Length];
         ys = new int[_input.Length];
     }
@@ -26,25 +28,65 @@ public class Day09 : BaseDay
         }
     }
 
+    long area(int x1, int y1, int x2, int y2) {
+        return (long)(Math.Abs(x1 - x2) + 1) * (long)(Math.Abs(y1 - y2) + 1);
+    }
+
     public long Part1(){
         parse();
         long max_size = 0;
-        for (int i = 0; i < _input.Length - 1; i++) {
-            for (int j = i+1; j < _input.Length; j++) {
-                max_size = Math.Max(max_size, (long)Math.Abs(xs[i] - xs[j] +1 )*(long)Math.Abs(ys[i] - ys[j] + 1));
+        for (int i = n/16; i < 3*n/16; i++) {
+            for (int j = 9*n/16; j < 11*n/16; j++ ) {
+                max_size = Math.Max(max_size, area(xs[i], ys[i], xs[j], ys[j]));
             }
         }
-
+        for (int i = 5*n/16; i < 7*n/16; i++) {
+            for (int j = 13*n/16; j < 15*n/16; j++ ) {
+                max_size = Math.Max(max_size, area(xs[i], ys[i], xs[j], ys[j]));
+            }
+        }
         return max_size;
     }
 
-    public int Part2(){
-        int min_x;
-        int max_x;
-        int min_y;
-        int max_y;
-        int delta;
+    public long Part2(){
+        int dx;
 
-        return 0;
+        int slitIndex = 2;
+        for (; slitIndex < n; slitIndex+=2) { // find index of slit
+            dx = xs[slitIndex] - xs[slitIndex-2];
+            if (dx > xs[0]/2) {
+                // Console.WriteLine("horizontal slit " + slitIndex + " has coords " + xs[slitIndex] + " " + ys[slitIndex]);
+                break;
+            }
+        }
+
+        int aboveIndex = 0;
+        for (; aboveIndex < n; aboveIndex+=2) {
+            if (xs[aboveIndex] < xs[slitIndex]) break;
+        }
+
+        long aboveRect = 0;
+        for (int i = slitIndex-2; i > 0; i--) {
+            if (ys[i] > ys[aboveIndex]) break;
+            if (xs[i] < xs[i+2]) continue;
+            aboveRect = Math.Max(aboveRect, area(xs[i], ys[i], xs[slitIndex], ys[slitIndex]));
+        }
+
+        int belowIndex = n-2;
+        for (; belowIndex > 0; belowIndex-=2) {
+            if (xs[belowIndex] < xs[slitIndex]) break;
+        }
+
+        long belowRect = 0;
+        for (int i = slitIndex+3; i < n; i++) {
+            if (ys[i] < ys[belowIndex]) break;
+            if (xs[i] < xs[i-2]) continue;
+            belowRect = Math.Max(belowRect, area(xs[i], ys[i], xs[slitIndex+1], ys[slitIndex+1]));
+        }
+
+        if (aboveRect > belowRect) {
+            return aboveRect;
+        }
+        return belowRect;
     }
 }
---
+++ AdventOfCode/Day10.cs
@@ -0,0 +1,84 @@
+﻿namespace AdventOfCode;
+
+public class Day10 : BaseDay
+{
+    private readonly string[] _input;
+    int n;
+    private int[] startingArrangements;
+    private List<int>[] buttons;
+
+    public Day10()
+    {
+        _input = File.ReadAllLines(InputFilePath);
+        n = _input.Length;
+        startingArrangements = new int[n];
+        buttons = new List<int>[n];
+    }
+
+    public override ValueTask<string> Solve_1() => new($"{Part1()}");
+
+    public override ValueTask<string> Solve_2() => new($"{Part2()}");
+
+    void parse() {
+        for (int i = 0; i < n; i++) {
+            string line = _input[i];
+            int j = 1;
+
+            // parse .# part
+            int arrangement = 0;
+            for (; j < line.Length; j++) {
+                if (line[j] == ']') break;
+                arrangement = arrangement << 1;
+                if (line[j] == '#') {
+                    arrangement ++;
+                }
+            }
+            startingArrangements[i] = arrangement;
+            int bitlength = j-2 +'0'; // not actually bitlength, but used to invert a digit 'n' to represent the nth bit counting from the left instead of the right
+
+            // parse numbers
+            List<int> listOfButtons = new List<int>();
+            int number = 0;
+            j+= 3;
+            for (; j < line.Length; j++) {
+                if (line[j] == '{') break;
+                else if (line[j] == ',') continue;
+                else if (line[j] == ')') {
+                    listOfButtons.Add(number);
+                    number = 0;
+                    j+=2;
+                } else {
+                    number += (1 << (bitlength - line[j]));
+                }
+            }
+            buttons[i] = listOfButtons;
+        }
+    }
+    public int Part1(){
+        parse();
+        int total = 0;
+        for (int i = 0; i < n; i++) {
+            // Console.WriteLine(i);
+            var results = new Dictionary<int, int> {{startingArrangements[i],0}};
+            foreach (var button in buttons[i]) {
+                // Console.WriteLine("testing button number value " + button);
+                var newResults = new Dictionary<int, int>(results);
+                foreach (var (result, mincount) in results) {
+                    int x = button ^ result;
+                    if (results.ContainsKey(x)) {
+                        newResults[x] = Math.Min(results[x], mincount+1);
+                    } else {
+                        newResults.Add(x,mincount+1);
+                    }
+                }
+                results = newResults;
+            }
+            total += results[0];
+        }
+        return total;
+    }
+
+    public int Part2(){
+        return 0;
+    }
+}
